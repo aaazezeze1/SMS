fd11c8d73bd5445c1b3d35b07251424e649ade1f
Amazing Cabiles <amazinggracecabiles@gmail.com>
2025-05-06 20:43:37 +0800

    added sidebar

---
+++ SMS/SMSapp/static/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggler = document.querySelector(".toggler-btn");
+
+    if (toggler) {
+        toggler.addEventListener("click", function(){
+            document.querySelector("#sidebar").classList.toggle("collapsed");
+        });
+    }
+});
